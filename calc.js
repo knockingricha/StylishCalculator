d9bc462305b8b6080020d07c9658a55205319d30
knockingricha <richabalupuri10@gmail.com>
2020-06-16 17:10:41 +0530

    calcu

---
+++ calc.js
@@ -0,0 +1,12 @@
+function insert(num)
+{
+    $('.calc-display').val($('.calc-display').val() + num);
+}
+function eql()
+{
+    $('.calc-display').val(eval($('.calc-display').val()));
+}
+function c()
+{
+    $('.calc-display').val('');
+}
